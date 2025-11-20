7fee56aca44bbe257d44af136db443f321ec9b1d
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-11-21 01:14:56 +0530

    update: minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,23 @@ import compression from "vite-plugin-compression";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        compression(), // Enable gzip compression for production
-        visualizer({
-            open: false, // Automatically opens the visualization report
-        }),
-    ],
-    // base: "/subhajit/", // Replace with your GitHub repository name
-    base: "/", // Replace with your GitHub repository name
-    build: {
-        outDir: "dist", // Output directory for production build
-        sourcemap: true, // Generate sourcemaps for debugging production code
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    // Example of chunk splitting
-                    vendor: ["react", "react-dom"],
-                },
-            },
+  plugins: [
+    react(),
+    compression(),
+    visualizer({
+      open: false,
+    }),
+  ],
+  base: "/",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
         },
+      },
     },
+  },
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
